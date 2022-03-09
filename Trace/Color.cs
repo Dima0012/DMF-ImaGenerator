8a8da93ca6735a4f385b90d66614a0b72c326433
Matteo Di Mario <mdima0012@gmail.com>
2022-03-09 12:24:13 +0100

    Try commit for merging

--- Trace/Color.cs
+++ Trace/Color.cs
@@ -1,16 +1,16 @@
 ﻿namespace Trace;
+
 public struct Color
 {
-   private float r { get; set; } = 0.0;
-   private float g { get; set; } = 0.0;
-   private float b { get; set; } = 0.0;
+   private float r { get; set; } = 0.;
+   private float g { get; set; } = 0.;
+   private float b { get; set; } = 0.;
 
-   public Colors( float r, float g, float b)
+   public Color(float r, float g, float b)
    {
       this.r = r;
       this.g = g;
       this.b = b;
    }
-   
 
 }
