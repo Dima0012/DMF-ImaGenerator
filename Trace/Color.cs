66368893b7a8127c18b5ec2b51d1bfebf0f12c4d
Matteo Di Mario <mdima0012@gmail.com>
2022-03-09 11:59:31 +0100

    Add r, g, b members and constructor

--- Trace/Color.cs
+++ Trace/Color.cs
@@ -1,5 +1,16 @@
 ﻿namespace Trace;
-public class Class1
+public struct Color
 {
+   private float r { get; set; } = 0.0;
+   private float g { get; set; } = 0.0;
+   private float b { get; set; } = 0.0;
+
+   public Colors( float r, float g, float b)
+   {
+      this.r = r;
+      this.g = g;
+      this.b = b;
+   }
+   
 
 }
