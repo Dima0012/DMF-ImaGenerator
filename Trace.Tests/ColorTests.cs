106a2c1612bc061d628e62d78987c124db30dad3
Francesca <francescamariaferrero@gmail.com>
2022-03-11 01:32:33 +0100

    Modified Color and added tests

--- Trace.Tests/ColorTests.cs
+++ Trace.Tests/ColorTests.cs
@@ -1,12 +1,46 @@
+using System;
 using Xunit;
+using Trace;
 
 namespace Trace.Tests;
 
 public class ColorTests
 {
     [Fact]
-    public void Test1()
-    {
 
+    public void TestClose()
+    {
+        Color a = new Color(1.0f, 2.0f, 3.0f);
+        Assert.True(a.is_close(new Color(1.0f, 2.0f, 3.0f)));
+        Assert.False(a.is_close(new Color(3.0f, 4.0f, 5.0f)));
+
+    }
+    [Fact]
+    public void TestAdd()
+    {
+        Color a1 = new Color(1.0f, 2.0f, 3.0f);
+        Color b1 = new Color(5.0f, 7.0f, 9.0f);
+        Assert.True((a1 + b1).is_close(new Color(6.0f, 9.0f, 12.0f)));
+    }
+    [Fact]
+    public void TestDiff()
+    {
+        Color a1 = new Color(1.0f, 2.0f, 3.0f);
+        Color b1 = new Color(5.0f, 7.0f, 9.0f);
+        Assert.True((a1 - b1).is_close(new Color(-4.0f, -5.0f, -6.0f)));
+    }
+    [Fact]
+    public void TestProd()
+    {
+        Color a1 = new Color(1.0f, 2.0f, 3.0f);
+        Color b1 = new Color(5.0f, 7.0f, 9.0f);
+        Assert.True((a1 * b1).is_close(new Color(5.0f, 14.0f, 27.0f)));
+    }
+    [Fact]
+    public void TestScalarProd()
+    {
+        Color a1 = new Color(1.0f, 2.0f, 3.0f);
+        Assert.True((a1*2.0f).is_close(new Color(2.0f, 4.0f, 6.0f)));
+        Assert.True((2.0f*a1).is_close(new Color(2.0f, 4.0f, 6.0f)));
     }
 }
--- Trace/Color.cs
+++ Trace/Color.cs
@@ -8,6 +8,12 @@ public struct Color
     private float G { get; set; }
     private float B { get; set; }
 
+    public Color()
+    {
+        R = 0.0f;
+        G = 0.0f;
+        B = 0.0f;
+    }
     public Color(float r, float g, float b)
     {
         R = r;
@@ -15,39 +21,57 @@ public struct Color
         B = b;
     }
 
-    public Color Add(Color col)
+    public static Color operator +(Color a, Color b)
     {
-        Color newCol = new Color(R + col.R, G + col.G, B + col.B);
+        Color newCol = new Color(a.R + b.R, a.G+ b.G, a.B + b.B);
         return newCol;
     }
 
-    public Color Diff(Color col)
+    public static Color operator -(Color a, Color b)
         // do we need absolute value?
     {
-        Color newCol = new Color(R - col.R, G - col.G, B - col.B);
+        Color newCol = new Color(a.R - b.R, a.G - b.G, a.B - b.B);
         return newCol;
     }
 
-    public Color Mul(Color col)
+    public static Color operator *(Color a, Color b)
     {
-        Color newCol = new Color(R * col.R, G * col.G, B * col.B);
+        Color newCol = new Color(a.R * b.R, a.G * b.G, a.B * b.B);
+        return newCol;
+    }
+    //vector- scalar product
+    public static Color operator *(Color a, float alpha)
+    {
+        Color newCol = new Color(a.R * alpha, a.G * alpha, a.B * alpha);
+        return newCol;
+    }
+    //scalar-vector product
+    public static Color operator *(float alpha, Color a)
+    {
+        Color newCol = new Color(a.R * alpha, a.G * alpha, a.B * alpha);
         return newCol;
     }
 
-    public Color Scalar_Mul(float alpha)
+    public bool is_close(Color a, double epsilon = 1e-5)
     {
-        Color newCol = new Color(R * alpha, G * alpha, B * alpha);
-        return newCol;
+        return Math.Abs(R - a.R) < epsilon && Math.Abs(G - a.G) < epsilon && Math.Abs(B - a.B) < epsilon;
     }
-
-    public bool are_close(double x, double y,
-            double epsilon = 1e-5) //should we put this function outside both the struct Color and the class HdrImage?
+    
+    public string to_string(Color a)
     {
-        return Math.Abs(x - y) < epsilon;
-    }
-
-    public bool are_colors_close(Color a, Color b)
-    {
-        return are_close(a.R, b.R) && are_close(a.G, b.G) && are_close(a.B, b.B);
+        return "r: " + a.R + ", g: " + a.G + ", b: " + a.B;
     }
 }
+
+/* Alternatively, are_close as functions
+public bool are_close(double x, double y,
+    double epsilon = 1e-5) //should we put this function outside both the struct Color and the class HdrImage?
+{
+    return Math.Abs(x - y) < epsilon;
+}
+
+public bool are_colors_close(Color a, Color b)
+{
+    return are_close(a.R, b.R) && are_close(a.G, b.G) && are_close(a.B, b.B);
+}
+*/
