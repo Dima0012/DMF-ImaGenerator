7c864774b862116918651f643c8e2e2cba66e4a3
Francesca <francescamariaferrero@gmail.com>
2022-03-22 11:29:46 +0100

    Deleted the trial I had done in the main

--- DMF-ImaGenerator/DMF-ImaGenerator.cs
+++ DMF-ImaGenerator/DMF-ImaGenerator.cs
@@ -4,21 +4,3 @@ using System.Text;
 using Trace;
 
 Console.WriteLine("Hello, World!");
-
-int[] a =
-{
-    1, 1, 2
-};
-
-int[] b =
-{
-    1, 1, 2
-};
-if (a[1] == b[1])
-{
-    Console.WriteLine("sono uguali");
-}
-else
-{
-    Console.WriteLine("Sono diversi");
-}
