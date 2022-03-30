0be399127ce555737484beb7c1ce7ef08da27d90
Matteo Di Mario <mdima0012@gmail.com>
2022-03-30 12:55:52 +0200

    Started working on Vec struct.

--- Trace/Geometry/Vec.cs
+++ Trace/Geometry/Vec.cs
@@ -1,1 +1,40 @@
 ﻿namespace Trace.Geometry;
+
+public struct Vec
+{
+    public float X { get; set; } = 0.0f;
+    public float Y { get; set; } = 0.0f;
+    public float Z { get; set; } = 0.0f;
+
+    public Vec(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+
+    public string to_string()
+    {
+        return $"< x: {X}, y: {Y}, z: {Z} >";
+    }
+
+
+    public bool is_close(Vec v, double epsilon = 10e-5)
+    {
+        return Math.Abs(X - v.X) < epsilon && Math.Abs(Y - v.Y) < epsilon && Math.Abs(Z - v.Z) < epsilon;
+    }
+
+    public static Vec operator+(Vec v, Vec u)
+    {
+        return new Vec(v.X + u.X, v.Y + u.Y, v.Z + u.Z);
+    }
+    
+    public static Vec operator-(Vec v, Vec u)
+    {
+        return new Vec(v.X - u.X, v.Y - u.Y, v.Z - u.Z);
+    }
+    
+    
+    
+}
