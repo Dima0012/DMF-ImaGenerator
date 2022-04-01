472cd49bbd412edc13d598256a124f630ca80781
Matteo Di Mario <mdima0012@gmail.com>
2022-04-01 16:12:20 +0200

    Added docstrings to Vec, Norm, Point.

--- Trace/Geometry/Normal.cs
+++ Trace/Geometry/Normal.cs
@@ -1,5 +1,8 @@
 ﻿namespace Trace.Geometry;
 
+/// <summary>
+/// A Normal Vector in 3D space.
+/// </summary>
 public struct Normal
 {
     public float X { get; set; } = 0.0f;
@@ -18,31 +21,49 @@ public struct Normal
         return $"Normal < x: {X}, y: {Y}, z: {Z} >";
     }
 
+    /// <summary>
+    /// Checks if two Normals are equal within a epsilon threshold.
+    /// </summary>
     public bool is_close(Normal n, double epsilon=10-5)
     {
         return Math.Abs(X - n.X) < epsilon && Math.Abs(Y - n.Y) < epsilon && Math.Abs(Z - n.Z) < epsilon;
     }
 
+    /// <summary>
+    /// Returns the negation of the Normal -n .
+    /// </summary>
     public Normal neg()
     {
         return new Normal(-X, -Y, -Z);
     }
 
+    /// <summary>
+    /// Overloading for product of a Normal and a scalar. 
+    /// </summary>
     public static Normal operator *(float a, Normal n)
     {
         return new Normal(a * n.X, a * n.Y, a * n.Z);
     }
     
+    /// <summary>
+    /// Overloading for product of a Normal and a scalar. 
+    /// </summary>
     public static Normal operator *(Normal n, float a)
     {
         return new Normal(a * n.X, a * n.Y, a * n.Z);
     }
 
+    /// <summary>
+    /// Overloading for the scalar product of two Vectors.
+    /// </summary>
     public static double operator *(Normal n, Normal m)
     {
         return n.X * m.X + n.Y * m.Y + n.Z * m.Z;
     }
     
+    /// <summary>
+    /// Returns the vector product of the Normal with the Vector v as a Vector.
+    /// </summary>
     public Vec vec_prod(Vec v)
     {
         return new Vec(
@@ -52,6 +73,9 @@ public struct Normal
         );
     }
     
+    /// <summary>
+    /// Returns the vector product of the Normal with the Normal n as a Vector.
+    /// </summary>
     public Vec vec_prod(Normal n)
     {
         return new Vec(
@@ -61,16 +85,25 @@ public struct Normal
         );
     }
     
+    /// <summary>
+    /// Returns the norm of the Normal.
+    /// </summary>
     public double norm()
     {
         return Math.Sqrt(squared_norm());
     }
 
+    /// <summary>
+    /// Returns the norm of the Normal.
+    /// </summary>
     public double squared_norm()
     {
         return X * X + Y * Y + Z * Z;
     }
 
+    /// <summary>
+    /// Normalize the Normal by invoking norm().
+    /// </summary>
     public void normalize()
     {
         X /= (float) norm();
--- Trace/Geometry/Vec.cs
+++ Trace/Geometry/Vec.cs
@@ -1,5 +1,8 @@
 ﻿namespace Trace.Geometry;
 
+/// <summary>
+/// A Vector in 3D space.
+/// </summary>
 public struct Vec
 {
     public float X { get; set; } = 0.0f;
@@ -19,7 +22,9 @@ public struct Vec
         return $"Vec < x: {X}, y: {Y}, z: {Z} >";
     }
 
-
+    /// <summary>
+    /// Checks if two Vectors are equal within a epsilon threshold.
+    /// </summary>
     public bool is_close(Vec v, double epsilon = 10e-5)
     {
         return Math.Abs(X - v.X) < epsilon && Math.Abs(Y - v.Y) < epsilon && Math.Abs(Z - v.Z) < epsilon;
@@ -35,26 +40,41 @@ public struct Vec
         return new Vec(v.X - u.X, v.Y - u.Y, v.Z - u.Z);
     }
 
+    /// <summary>
+    /// Overloading for product of a Vector and a scalar. 
+    /// </summary>
     public static Vec operator *(Vec v, float alpha)
     {
         return new Vec(v.X * alpha, v.Y * alpha, v.Z * alpha);
     }
 
+    /// <summary>
+    /// Overloading for product of a Vector and a scalar. 
+    /// </summary>
     public static Vec operator *(float alpha, Vec v)
     {
         return new Vec(v.X * alpha, v.Y * alpha, v.Z * alpha);
     }
 
+    /// <summary>
+    /// Returns the negation of the Vector -v .
+    /// </summary>
     public Vec neg()
     {
         return new Vec(-X, -Y, -Z);
     }
 
+    /// <summary>
+    /// Overloading for the scalar product of two Vectors.
+    /// </summary>
     public static double operator *(Vec v, Vec u)
     {
         return v.X * u.X + v.Y * u.Y + v.Z * u.Z;
     }
 
+    /// <summary>
+    /// Returns the vector product of the Vector with the Vector v.
+    /// </summary>
     public Vec vec_prod(Vec v)
     {
         return new Vec(
@@ -64,16 +84,25 @@ public struct Vec
         );
     }
 
+    /// <summary>
+    /// Returns the norm of the Vector.
+    /// </summary>
     public double norm()
     {
         return Math.Sqrt(squared_norm());
     }
 
+    /// <summary>
+    /// Returns the squared norm of the Vector.
+    /// </summary>
     public double squared_norm()
     {
         return X * X + Y * Y + Z * Z;
     }
 
+    /// <summary>
+    /// Normalize the Vector by invoking norm().
+    /// </summary>
     public void normalize()
     {
         X /= (float) norm();
