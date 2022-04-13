455e4455db1790281939d4c3cbe27946def54937
Francesca <francescamariaferrero@gmail.com>
2022-04-13 12:25:39 +0200

    Done struct Ray.cs and its tests

--- Trace.Tests/RayTests.cs
+++ Trace.Tests/RayTests.cs
@@ -1,10 +1,30 @@
 using System;
 using Xunit;
 using Trace;
+using Trace.Cameras;
+using Trace.Geometry;
 
 namespace Trace.Tests;
 
 public class RayTests
 {
-   
+    [Fact]
+    public void TestIsClose()
+    {
+        var ray1 = new Ray(new Point(1.0f, 2.0f, 3.0f), new Vec(5.0f, 4.0f, -1.0f));
+        var ray2 = new Ray(new Point(1.0f, 2.0f, 3.0f), new Vec(5.0f, 4.0f, -1.0f));
+        var ray3 = new Ray(new Point(5.0f, 1.0f, 4.0f), new Vec(3.0f, 9.0f, 4.0f));
+        
+        Assert.True(ray1.is_close(ray2));
+        Assert.False(ray1.is_close(ray3));
+    }
+
+    [Fact]
+    public void TestAt()
+    {
+        var ray = new Ray(new Point(1.0f, 2.0f, 4.0f), new Vec(4.0f, 2.0f, 1.0f));
+        Assert.True(ray.at(0.0f).is_close(ray.Origin));
+        Assert.True(ray.at(1.0f).is_close(new Point(5.0f, 4.0f, 5.0f)));
+        Assert.True(ray.at(2.0f).is_close(new Point(9.0f, 6.0f, 6.0f)));
+    }
 }
--- Trace/Cameras/Ray.cs
+++ Trace/Cameras/Ray.cs
@@ -1,6 +1,37 @@
-﻿namespace Trace.Cameras;
+﻿using Trace.Geometry;
+
+namespace Trace.Cameras;
 
 public struct Ray
 {
-    
+    public Point Origin { get; set; }
+    public Vec Dir { get; set; }
+    public float Tmin { get; set; } = 1e-5f;
+    public float Tmax { get; set; } = float.PositiveInfinity;
+    public int Depth { get; set; } = 0;
+
+    public Ray(Point origin, Vec dir, float tmin, float tmax, int depth)
+    {
+        Origin = origin;
+        Dir = dir;
+        Tmin = tmin;
+        Tmax = tmax;
+        Depth = depth;
+    }
+
+    public Ray(Point origin, Vec dir)
+    {
+        Origin = origin;
+        Dir = dir;
+    }
+
+    public Point at(float t)
+    {
+        return Origin + Dir * t;
+    }
+
+    public bool is_close(Ray ray, double epsilon = 1e-5)
+    {
+        return Origin.is_close(ray.Origin, epsilon) && Dir.is_close(ray.Dir, epsilon);
+    }
 }
