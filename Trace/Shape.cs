2b0bd79c09896821187900d60684cfb5ae174519
Francesca <francescamariaferrero@gmail.com>
2022-04-27 23:20:59 +0200

    First commit _ started the implementation of Shape.cs

---
+++ Trace/Shape.cs
@@ -0,0 +1,25 @@
+﻿using Trace.Geometry;
+using Trace.Cameras;
+
+
+namespace Trace;
+
+/// <summary>
+/// Abstract class that represents a shape.
+/// </summary>
+abstract class Shape
+{
+    protected Transformation Transformation;
+    
+    protected Shape()
+    {
+        Transformation = new Transformation();
+    }
+
+    protected Shape(Transformation transformation)
+    {
+        Transformation = transformation;
+    }
+
+    public abstract HitRecord? ray_intersection(Ray ray);
+}
