4081bc5b0d01237e4436eec4ea14a1079e538d5e
Matteo Di Mario <mdima0012@gmail.com>
2022-04-27 12:54:12 +0200

    Modified test ImageTracerTests.cs. Now bug is correctly identified.

--- Trace.Tests/ImageTracerTests.cs
+++ Trace.Tests/ImageTracerTests.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using Xunit;
 using Trace.Cameras;
 using Trace.Geometry;
@@ -6,17 +7,28 @@ namespace Trace.Tests;
 
 public class ImageTracerTests
 {
-    [Fact]
-    public void TestImageTracer()
-    {
-        var image = new HdrImage(4, 2);
-        var cam = new PerspectiveCamera(2.0f, new Transformation());
-        var tracer = new ImageTracer(image, cam);
+    public HdrImage image;
+    public PerspectiveCamera cam;
+    public ImageTracer tracer;
 
+    public ImageTracerTests()
+    {
+        image = new HdrImage(4, 2);
+        cam = new PerspectiveCamera(2.0f, new Transformation());
+        tracer = new ImageTracer(image, cam);
+    }
+
+    [Fact]
+    public void TestUvSubMapping()
+    {
         var ray1 = tracer.fire_ray(0, 0, 2.5f, 1.5f);
         var ray2 = tracer.fire_ray(2, 1, 0.5f, 0.5f);
         Assert.True(ray1.is_close(ray2));
-        
+    }
+
+    [Fact]
+    public void TestImageCoverage()
+    {
         tracer.fire_all_rays(_ => new Color(1f, 2f, 3f));
         for (int row = 0; row < image.Height; row++)
         {
@@ -25,5 +37,15 @@ public class ImageTracerTests
                 Assert.True(image.get_pixel(col, row).is_close(new Color(1f, 2f, 3f)));
             }
         }
+    }
+
+    [Fact]
+    public void TestOrientation()
+    {
+        var topLeftRay = tracer.fire_ray(0,0, 0.0f, 0.0f);
+        Assert.True(new Point(0f,2f,1f).is_close(topLeftRay.at(1f)));
+
+        var bottomRightRay = tracer.fire_ray(3, 1, 1f, 1f);
+        Assert.True(new Point(0f,-2f,-1f).is_close(bottomRightRay.at(1f)));
     }
 }
