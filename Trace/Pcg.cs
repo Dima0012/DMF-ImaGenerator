30435c4b1682bef464a330aac67cfc897ae2c3af
Matteo Di Mario <mdima0012@gmail.com>
2022-05-11 11:40:22 +0200

    Added random and random_float.

--- Trace/Pcg.cs
+++ Trace/Pcg.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace Trace;
 
 public class Pcg
@@ -13,5 +15,28 @@ public class Pcg
         State += init_state;
         random();       //Throw a random number and discard it
 
+    }
+
+
+    public uint random()
+    {
+        // 64-bit
+        ulong oldState = State;
+        State = (ulong) (oldState * 6364136223846793005 + Inc);
+        
+        // 32-bit
+        uint xorShifted = (uint) (((oldState >> 18) ^ oldState) >> 27);
+        uint rot = (uint) (oldState >> 59);
+
+        return (uint) (xorShifted >> rot) | (xorShifted << ((-rot) & 31));
+    }
+    
+    /// <summary>
+    /// Returns a new floating-point random number in [0,1) and
+    /// advance PCG's internal state.
+    /// </summary>
+    public float random_float()
+    {
+        return random() / 0xffffffff;
     }
 }
