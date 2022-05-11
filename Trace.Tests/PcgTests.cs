5a6dcc9f42d5d86eadd6e42fa24686870af04397
Matteo Di Mario <mdima0012@gmail.com>
2022-05-11 11:13:38 +0200

    Added Pcg Test.

--- Trace.Tests/PcgTests.cs
+++ Trace.Tests/PcgTests.cs
@@ -1,6 +1,28 @@
+using Xunit;
+
 namespace Trace.Tests;
 
 public class PcgTests
 {
-    
+    [Fact]
+    public void TestRandom()
+    {
+        var pcg = new Pcg();
+
+        Assert.True(pcg.State == 1753877967969059832);
+        Assert.True(pcg.inc == 109);
+
+        uint[] testRndVec =
+        {
+            2707161783, 2068313097,
+            3122475824, 2211639955,
+            3215226955, 3421331566
+        };
+
+        foreach (var expected in testRndVec)
+        {
+            var result = pcg.random();
+            Assert.True(expected == result);
+        }
+    }
 }
