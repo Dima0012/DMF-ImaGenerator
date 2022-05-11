8d0a4cc4dffebf625cc8fd1d7b6f60ae81298625
Francesca <francescamariaferrero@gmail.com>
2022-05-11 11:34:06 +0200

    Done pcg constructor

--- Trace/Pcg.cs
+++ Trace/Pcg.cs
@@ -2,5 +2,16 @@ namespace Trace;
 
 public class Pcg
 {
-    
+    public ulong State { get; set; }
+    public ulong Inc { get; set; }
+
+    public Pcg(ulong init_state = 42, ulong init_seq = 54)
+    {
+        State = 0;
+        Inc = (init_seq << 1) | 1;
+        random();       //Throw a random number and discard it
+        State += init_state;
+        random();       //Throw a random number and discard it
+
+    }
 }
